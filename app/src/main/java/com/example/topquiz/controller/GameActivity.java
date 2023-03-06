4a2602957096e2cfd487a0daa3d0aca2020bee28
Benjamin PERRIN <benjaminperrinpro3@gmail.com>
2023-03-06 17:06:22 +0100

    test

---
+++ app/src/main/java/com/example/topquiz/controller/GameActivity.java
@@ -0,0 +1,20 @@
+package com.example.topquiz.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.topquiz.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    public void sendMessage(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/topquiz/model/model/user.java
@@ -0,0 +1,13 @@
+/*package com.example.topquiz.model.model;
+
+public class user {
+    private string mFirstName;
+
+    public string getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(string firstName) {
+        mFirstName = firstName;
+    }
+}*/
